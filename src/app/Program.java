7ba67561c4bec41c087c4e748e90058024a54cae
JeanButtei <jeanbuttei@gmail.com>
2020-11-12 17:28:32 -0300

    Starting Board and Piece

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,13 @@
 package app;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(0, 7);
-		
-		System.out.println(pos);
+		Board board = new Board(8, 8);
 	}
 
 }
