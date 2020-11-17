db0ccd4e414975efc8cfb22043e0412dac1a86b4
JeanButtei <jeanbuttei@gmail.com>
2020-11-17 15:58:39 -0300

    Rook possible moves

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,8 +18,55 @@ public class Rook extends ChessPiece {
 
 	@Override
 	public boolean[][] possibleMoves() {		
-		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		return mat;
+		boolean[][] moves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position initialPosition = new Position(0, 0);
+		
+		//Above
+		initialPosition.setValues(position.getRow() - 1, position.getColumn());
+		while ((getBoard().positionExists(initialPosition)) && (!getBoard().thereIsAPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+			initialPosition.setRow(initialPosition.getRow() - 1);
+		}
+		
+		if ((getBoard().positionExists(initialPosition)) && (isThereOpponentPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+		}
+		
+		//Left
+		initialPosition.setValues(position.getRow(), position.getColumn() - 1);
+		while ((getBoard().positionExists(initialPosition)) && (!getBoard().thereIsAPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+			initialPosition.setColumn(initialPosition.getColumn() - 1);
+		}
+
+		if ((getBoard().positionExists(initialPosition)) && (isThereOpponentPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+		}
+		
+		//Right
+		initialPosition.setValues(position.getRow(), position.getColumn() + 1);
+		while ((getBoard().positionExists(initialPosition)) && (!getBoard().thereIsAPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+			initialPosition.setColumn(initialPosition.getColumn() + 1);
+		}
+
+		if ((getBoard().positionExists(initialPosition)) && (isThereOpponentPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+		}
+		
+		//Below
+		initialPosition.setValues(position.getRow() + 1, position.getColumn());
+		while ((getBoard().positionExists(initialPosition)) && (!getBoard().thereIsAPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+			initialPosition.setRow(initialPosition.getRow() + 1);
+		}
+
+		if ((getBoard().positionExists(initialPosition)) && (isThereOpponentPiece(initialPosition))) {
+			moves[initialPosition.getRow()][initialPosition.getColumn()] = true;
+		}
+		
+		return moves;
 	}
 
 }
